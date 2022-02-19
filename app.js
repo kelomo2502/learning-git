da9933ad2d0b94a616e8ef59b78bc43368c404b4
Oyewunmi Gbenga kelvin <kelvinoye@gmail.com>
2022-02-19 15:51:52 +0100

    micros

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+function addNum(a,b){
+   return a + b
+}
+console.log(addNum(29,21))
+function greetGuest(name, age) {
+   return `Hello ${name} , you are ${age} years old`
+}
+console.log(greetGuest("Gbenga", 40))
